b04bb49f6373b0f7363d5fbd9926d6a160d53524
rajat4sharma <rajat.sharma@zemosolabs.com>
2022-05-26 22:24:39 +0530

    merge

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,18 @@ import bodyParser from 'body-parser';
 
 import {productsRouter} from './routes/todos';
 import {connectToDatabase} from "./helpers/db_client"
-
+import cors from 'cors';
 const app = express();
 
 app.use(bodyParser.json());
 
 connectToDatabase()
     .then(() => {
+        app.use(cors({
+            origin: '*'
+        }));
         app.use("/products", productsRouter);
-
+       
         app.listen(3000, () => {
             console.log(`Server started at http://localhost:${3000}`);
         });
